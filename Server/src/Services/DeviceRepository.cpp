34b204d3bb0adeecb1a41f33830d712398ff6913
Oliver <oliver@seidel-it.com>
2024-10-27 14:02:14 +0100

    Integrate boost.test

---
+++ Server/src/Services/DeviceRepository.cpp
@@ -0,0 +1,18 @@
+  #include "DeviceRepository.h"
+
+  void DeviceRepository::Load()
+  {
+
+  }
+  void DeviceRepository::Save()
+  {
+
+  }
+  void DeviceRepository::Add( const SDevice& device )
+  {
+
+  }
+  void DeviceRepository::Delete( const SDevice& device )
+  {
+
+  }
